6c5d6163bc3afe10b121e2ed6e79e89898c37967
Diego <estradadiego99@gmail.com>
2019-02-02 02:06:41 -0600

    Agregando un problema

---
+++ LC771.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+
+// Diego Estrada Talamantes
+// devdiegoest@gmail.com
+
+public class LC771{
+
+    public static void main(String[] args) {
+        long startTime = System.nanoTime();
+        // Aquí escribimos el método respuesta
+        System.out.println();
+        long endTime   = System.nanoTime();
+        long totalTime = (endTime - startTime) / 1000000;
+        System.out.println("Tiempo: "+totalTime+" ms");
+    }
+
+    public int numJewelsInStones(String J, String S) {
+        HashSet<Character> piedras = new HashSet<>();
+        int c = 0;
+        for(int i=0; i<J.length(); i++){
+            piedras.add(J.charAt(i));
+        }
+        for(int i=0; i<S.length(); i++){
+            if(piedras.contains(S.charAt(i))){
+                c++;
+            }
+        }
+        return c;
+    }
+    
+}
